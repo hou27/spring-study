a1f23c17477aec8ed7e2001586ba3d61d21df0ce
hou27 <ataj125@gmail.com>
2022-05-02 00:51:52 +0900

    Test code

---
+++ src/test/java/prac/prac_spring/repository/MemoryUserRepositoryTest.java
@@ -0,0 +1,61 @@
+package prac.prac_spring.repository;
+
+import static org.assertj.core.api.Assertions.*; // static import
+
+//import org.assertj.core.api.Assertions;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import prac.prac_spring.domain.User;
+
+class MemoryUserRepositoryTest {
+
+  MemoryUserRepository memoryUserRepository = new MemoryUserRepository();
+
+  @Test
+  public void save() {
+    User user = new User();
+    user.setName("testuser");
+
+    memoryUserRepository.save(user);
+
+    User result = memoryUserRepository.findById(user.getId()).get();
+    System.out.println("result = " + (result == user));
+//    Assertions.assertEquals(user, result);
+//    assertThat(result).isEqualTo(null);
+    assertThat(result).isEqualTo(user);
+  }
+
+  @Test
+  public void findByName() {
+
+    User user1 = new User();
+    user1.setName("test1");
+    memoryUserRepository.save(user1);
+
+    User user2 = new User();
+    user2.setName("test2");
+    memoryUserRepository.save(user2);
+
+    User result = memoryUserRepository.findByName("test1").get();
+    assertThat(result).isEqualTo(user1);
+  }
+
+  @Test
+  public void findAll() {
+    //given
+    User user1 = new User();
+    user1.setName("test1");
+    memoryUserRepository.save(user1);
+    User user2 = new User();
+    user2.setName("test2");
+    memoryUserRepository.save(user2);
+
+    //when
+    List<User> result = memoryUserRepository.findAll();
+
+    //then
+    assertThat(result.size()).isEqualTo(2);
+  }
+
+}
