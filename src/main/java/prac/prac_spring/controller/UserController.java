6e4eb93214b15fa9a9192aba404c6cfbfdb70208
hou27 <ataj125@gmail.com>
2022-05-03 16:35:44 +0900

    Hand over to View using Spring Model interface

--- src/main/java/prac/prac_spring/controller/UserController.java
+++ src/main/java/prac/prac_spring/controller/UserController.java
@@ -1,7 +1,9 @@
 package prac.prac_spring.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import prac.prac_spring.domain.User;
@@ -31,5 +33,17 @@ public class UserController {
     userService.signUp(user);
 
     return "redirect:/";
+  }
+
+  @GetMapping("/users")
+  public String showUserList(Model model) {
+    List<User> userList = userService.findAllUsers();
+    /**
+     * model에 user 객체 list에 대한 정보를 담는다.
+     * 이를 통해 private 요소에 접근이 가능해진다.(getter와 setter가 정의되어있을 시)
+     */
+    model.addAttribute("userList", userList);
+
+    return "users/userList";
   }
 }
